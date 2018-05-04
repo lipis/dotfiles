f7e038d43edc5e19f0ac5c3fdc40dac6f6deb1a6
Panayiotis Lipiridis <lipiridis@gmail.com>
2018-05-04 16:12:40 +0200

    Update pip

--- pip.sh
+++ pip.sh
@@ -2,10 +2,9 @@
 
 pip install babel
 pip install boto3
-pip install cryptography
-pip install flake8
 pip install flask
 pip install flask-babel
 pip install requests
 pip install virtualenv
 pip install yapf
+pip install pur
